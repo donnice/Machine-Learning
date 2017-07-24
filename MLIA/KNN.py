d540bce4ad822efd88f40c60fbe30c6fc3a71df7
Hai Tang <402696415@qq.com>
2017-07-24 00:21:45 -0700

    KNN, p24

--- MLIA/KNN.py
+++ MLIA/KNN.py
@@ -7,4 +7,17 @@ def createDataSet():
 	return group, labels
 
 def classify0(inX, dataSet, labels, k):
-	pass
+	dataSetSize = dataSet.shape[0]
+	diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+	sqDiffMat = diffMat**2
+	# add each line
+	# for example, [[0,1,2],[2,1,3]] => [3,6]
+	sqDistances = sqDiffMat.sum(axis=1)
+	distances = sqDistances**0.5
+	sortedDistIndicies = distances.argsort()
+	classCount = {}
+	for i in range(k):
+		voteIlabel = labels[sortedDistIndicies[i]]
+		classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+	sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+	return sortedClassCount[0][0]
