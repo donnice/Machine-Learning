b54185c49745e020851285ecdccb0793e64e3da4
Hai Tang <402696415@qq.com>
2017-07-29 16:09:12 -0700

    file2matrix

--- MLIA/KNN.py
+++ MLIA/KNN.py
@@ -35,10 +35,21 @@ def classify0(inX, dataSet, labels, k):
 	sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
 	return sortedClassCount[0][0]
 
-	def file2matrix(filename):
-		fr = open(filename)
-		numberOfLines = len(fr.readlines())
-		returnMat = zeros((numberOfLines, 3))
-		classLabelVector = []
-		fr = open(filename)
-		
+def file2matrix(filename):
+	fr = open(filename)
+	numberOfLines = len(fr.readlines()) # Get # of lines
+	returnMat = zeros((numberOfLines, 3)) # Create Numpy matrix to return
+	classLabelVector = []
+	fr = open(filename)
+	# Parse line to a list
+	index = 0
+	for line in fr.readlines():
+		line = line.strip() # remove all whitespaces at start and end
+		listFromLine = line.split('\t') # \t is a tab or 6 spaces
+		returnMat[index, :] = listFromLine[0:3]
+		# print returnMat
+		classLabelVector.append(listFromLine[-1])
+		index += 1
+	return returnMat, classLabelVector
+			
+
