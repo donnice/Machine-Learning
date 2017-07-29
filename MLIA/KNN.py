92a031d84c1cd969ee716ca6159a0844520afeaa
Hai Tang <402696415@qq.com>
2017-07-29 14:00:05 -0700

    improve listing 2.1

--- MLIA/KNN.py
+++ MLIA/KNN.py
@@ -1,12 +1,22 @@
+# Listing 2.1
 from numpy import *
 import operator
 
 def createDataSet():
-	group = array([1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1])
-	labels = ['A', 'A', 'B', 'B']
+	group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+	labels = ['A', 'B', 'C', 'D']
 	return group, labels
 
 def classify0(inX, dataSet, labels, k):
+	'''
+	Distance calculation
+	For every point in dataset:
+		calculate distance between inX and the current point
+		sort distances in increasing order
+		take k items with lowest distances to inX
+		find the majority class among these items
+		return majority class as our prediction for class of inX
+	'''
 	dataSetSize = dataSet.shape[0]
 	diffMat = tile(inX, (dataSetSize, 1)) - dataSet
 	sqDiffMat = diffMat**2
@@ -15,6 +25,9 @@ def classify0(inX, dataSet, labels, k):
 	sqDistances = sqDiffMat.sum(axis=1)
 	distances = sqDistances**0.5
 	sortedDistIndicies = distances.argsort()
+	'''
+	Voting with lowest k distances
+	'''
 	classCount = {}
 	for i in range(k):
 		voteIlabel = labels[sortedDistIndicies[i]]
