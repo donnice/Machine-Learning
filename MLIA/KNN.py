b77198e982b6a95dc5f7f06f0593e67d10dd9b8b
Hai Tang <402696415@qq.com>
2017-07-31 22:17:43 -0700

    classify tester

--- MLIA/KNN.py
+++ MLIA/KNN.py
@@ -54,6 +54,12 @@ def file2matrix(filename):
 	return returnMat, classLabelVector
 
 def autoNorm(dataSet):
+	'''
+	>> tile(5,(3,2))
+	array([[5, 5],
+       [5, 5],
+       [5, 5]])
+	'''
 	minVals = dataSet.min(0)
 	maxVals = dataSet.max(0)
 	ranges = maxVals - minVals
@@ -62,3 +68,19 @@ def autoNorm(dataSet):
 	normDataSet = dataSet - tile(minVals, (m, 1))
 	normDataSet = normDataSet/tile(ranges, (m, 1))	
 	return normDataSet, ranges, minVals
+
+def datingClassTest():
+	hoRatio = 0.10
+	datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	numTestVecs = int(m*hoRatio)
+	errorCount = 0.0
+	for i in range(numTestVecs):
+		classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], \
+					datingLabels[numTestVecs: m], 3)
+		print "the classifier came back with: %s, the real answer is: %s" \
+					% (classifierResult, datingLabels[i])
+		if (classifierResult != datingLabels[i]):
+			errorCount += 1.0
+	print "The total error rate is: %f" % (errorCount/float(numTestVecs))
