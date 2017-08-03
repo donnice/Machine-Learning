e150208744c87a05a3f66fd7d3fa76cf546c1f3f
Hai Tang <402696415@qq.com>
2017-08-02 23:02:32 -0700

    pesudo code for dt

--- MLIA/KNN.py
+++ MLIA/KNN.py
@@ -1,3 +1,14 @@
+'''
+1. Collect: Any method.
+2. Prepare: Numeric values are needed for a distance calculation. A structured data format is best.
+3. Analyze: Any method.
+4. Train: Does not apply to the kNN algorithm.
+5. Test: Calculate the error rate.
+6. Use: This application needs to get some input data and output structured num- eric values. 
+Next, the application runs the kNN algorithm on this input data and determines which class the input data should belong to. 
+The application then takes some action on the calculated class.
+'''
+
 from numpy import *
 import operator
 import os
