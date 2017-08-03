418190a30d6f543c570c0710d51b9b01cdabb7fa
Hai Tang <402696415@qq.com>
2017-08-02 23:58:52 -0700

    shannon entropy

--- MLIA/DecisionTree.py
+++ MLIA/DecisionTree.py
@@ -13,4 +13,15 @@ Check if every item in the dataset is in the same class:
 
 from math import log
 
-def  
+# H = -Sum(1,n)(p(xi)log(p(xi))), were p(xi) is the prob of choosing this class
+def calShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        labelCounts[currentLabel] = labelCounts.get(labelCounts, 0) + 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
