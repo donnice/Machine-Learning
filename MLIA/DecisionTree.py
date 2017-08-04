82544f73e9b2bd95ee057530d984f97b81d3d3a1
Hai Tang <402696415@qq.com>
2017-08-03 23:47:27 -0700

    cheater

--- MLIA/DecisionTree.py
+++ MLIA/DecisionTree.py
@@ -25,3 +25,12 @@ def calShannonEnt(dataSet):
         prob = float(labelCounts[key])/numEntries
         shannonEnt -= prob * log(prob, 2)
     return shannonEnt
+
+def createDataSet():
+    dataSet = [[1, 1, 'Yes'],
+            [1, 1, 'Yes'],
+            [1, 0, 'No'],
+            [0, 1, 'No'],
+            [0, 1, 'No']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
