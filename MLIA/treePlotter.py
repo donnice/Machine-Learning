ce3018ed449e276af2d492b494c9d4fcd60b3a21
Hai Tang <402696415@qq.com>
2017-08-06 23:17:50 -0700

    plotter

--- MLIA/treePlotter.py
+++ MLIA/treePlotter.py
@@ -5,13 +5,13 @@ decisionNode = dict(boxstyle="sawtooth", fc="0.8")
 leafNode = dict(boxstyle="round4", fc="0.8")
 arrow_args = dict(arrowstyle="<-")
 
-def createPlot():
-    fig = plt.figure(1, facecolor='white')
-    fig.clf()
-    createPlot.ax1 = plt.subplot(111, frameon=False)
-    plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
-    plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
-    plt.show()
+# def createPlot():
+#     fig = plt.figure(1, facecolor='white')
+#     fig.clf()
+#     createPlot.ax1 = plt.subplot(111, frameon=False)
+#     plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
+#     plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
+#     plt.show()
 
 def plotNode(nodeTxt, centerPt, parentPt, nodeType):
     createPlot.ax1.annotate(nodeTxt, xy=parentPt, 
@@ -31,3 +31,45 @@ def getNumLeafs(myTree):
             numLeafs += 1
     return numLeafs
 
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    return maxDepth
+
+def retrieveTree(i):
+    listOfTrees = [{'no surfacing': {0: 'no', 1: {'flippers': \
+                    {0: 'no', 1: 'yes'}}}},
+                    {'no surfacing': {0: 'no', 1: {'flippers': \
+                    {0: {'head':{0: 'no', 1: 'yes'}}, 1: 'yes'}}}}]
+    return listOfTrees[i]
+
+# Plots text between child and parent
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0])/2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1])/2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodetxt):
+    numLeafs = getNumLeafs(myTree)
+    getTreeDepth(myTree)
+    pass
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.show()
