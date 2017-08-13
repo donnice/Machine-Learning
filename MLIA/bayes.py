ca1a8fbe7576f799bee0e269ceccd87f3394a7c2
Hai Tang <402696415@qq.com>
2017-08-13 00:42:56 -0700

    fix bug in localData, add logistic regression

--- MLIA/bayes.py
+++ MLIA/bayes.py
@@ -136,8 +136,7 @@ def spamTest():
         wordVector = setOfWords2Vec(vocabList, docList[docIndex])
         if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
             errorCount += 1
-    print trainMat[0][:10]
-    print trainClasses[:30]
+    
     print 'The error rate is: ', float(errorCount)/len(testSet)
 
 # Using Naive Bayes to find locally used words
@@ -191,7 +190,7 @@ def localWords(feed1, feed0):
         # frequency of each word
         trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
         
-        trainClasses.append(classList[randIndex])
+        trainClasses.append(classList[docIndex])
     p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
     errorCount = 0
     for docIndex in testSet:
@@ -201,3 +200,21 @@ def localWords(feed1, feed0):
             errorCount += 1
     print 'the error rate is: ', float(errorCount)/len(testSet)
     return vocabList, p0V, p1V
+
+def getTopWords(ny, sf):
+    import operator
+    vocabList, p0V, p1V = localWords(ny, sf)
+    topNY = []; topSF = []
+    for i in range(len(p0V)):
+        if p0V[i] > -6.0: topSF.append((vocabList[i], p0V[i]))
+        if p1V[i] > -6.0: topNY.append((vocabList[i], p1V[i]))
+    sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
+    print "SF*SF*SF*SF*SF*SF*SF*SF*SF*SF*SF*SF*S*FS*FSF*SF*SF*SF"
+    for item in sortedSF:
+        print item[0]
+
+    print "NY*NY*Y*Y*NY*NY*YNY*NY*NY*NY*NY*NY*NY*NY*NY*N*NY*NY*N"
+    sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
+    for item in sortedNY:
+        print item[0]
+    
---
+++ MLIA/logRegres.py
@@ -0,0 +1,9 @@
+# General approach to logistic regression
+'''
+1. Collect
+2. Prepare: Numeric values are needed for distance calculation
+3. Analyze
+4. Train: Try to find optimal coefficients to classify the data
+5. Test
+6. Use - regression calculation
+'''
