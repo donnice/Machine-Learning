61fc3281c21bfb164efb8e372def229099f2c58f
Hai Tang <402696415@qq.com>
2017-08-13 21:46:44 -0700

    logistic regression

--- MLIA/logRegres.py
+++ MLIA/logRegres.py
@@ -8,12 +8,13 @@
 6. Use - regression calculation
 
 sigmoid function:
-σ(z) = 1/(1+e(-z))
+sigma(z) = 1/(1+e(-z))
 z = w0x0 + w1x1 + ... + wnxn
 
 Gradient Ascent algorithm:
-w: = w + α(Gradient)wf(w)
+w: = w + alpha(Gradient)wf(w)
 '''
+from numpy import *
 
 # Logistic regression gradient ascent optimization functions
 def loadDataSet():
@@ -24,3 +25,46 @@ def loadDataSet():
         dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
         labelMat.append(int(lineArr[2]))
     return dataMat, labelMat
+
+def sigmoid(inX):
+    return 1.0/(1+exp(-inX))
+
+def gradAscent(dataMatIn, classLabels):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    m, n = shape(dataMatrix)
+    alpha = 0.001
+    maxCycles = 500
+    weights = ones((n, 1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix*weights)
+        error = labelMat - h
+        weights = weights + alpha * dataMatrix.transpose()*error
+    return weights
+
+# Plotting the logistic regression best-fit line and dataset
+def plotBestFit(wei):
+    import matplotlib.pyplot as plt
+    dataMat, labelMat = loadDataSet()
+    weights = wei.getA()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1])
+            ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
