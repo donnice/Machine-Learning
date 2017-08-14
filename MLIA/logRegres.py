73d5f2737cb633623e9c540b2eb594ab0e556af1
Hai Tang <402696415@qq.com>
2017-08-13 20:05:58 -0700

    loadDataSet

--- MLIA/logRegres.py
+++ MLIA/logRegres.py
@@ -6,4 +6,21 @@
 4. Train: Try to find optimal coefficients to classify the data
 5. Test
 6. Use - regression calculation
+
+sigmoid function:
+σ(z) = 1/(1+e(-z))
+z = w0x0 + w1x1 + ... + wnxn
+
+Gradient Ascent algorithm:
+w: = w + α(Gradient)wf(w)
 '''
+
+# Logistic regression gradient ascent optimization functions
+def loadDataSet():
+    dataMat = []; labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
