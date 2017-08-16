8ee39e76b580fbfb64636f93029649a059480f08
Hai Tang <402696415@qq.com>
2017-08-15 22:37:48 -0700

    theory explanation

--- MLIA/svmMLiA.py
+++ MLIA/svmMLiA.py
@@ -1,3 +1,14 @@
+'''
+SVM: The best margin (seperating line in 2-D plan) of points
+Suport vectors: Points closet to hyperplane
+Point perpendicular to line: |w(T)x+b|/||w||
+label: 1 or -1, the nearest point(s)
+margin maximum with points with smallest margin:
+    argmax(w,b){min(label*(w(T)x+b))/||w||}
+=> Lagrange =>
+    max(a)[sigma(1, m){a} - sigma(i,j=1,, m){label(i)*label(j)*ai*aj*<x(i),x(j)}/2]
+    with a>=0 and sigma(i-1, m){ai*label(i)} = 0
+'''
 # Helper function for SMO (Sequential Minimal Optimization) algorithm
 def loadDataSet(fileName):
     dataMat = []
