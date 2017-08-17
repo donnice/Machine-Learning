026bf433b7e99e3a6ec333eb49a49a1620ed161e
Hai Tang <402696415@qq.com>
2017-08-16 23:28:20 -0700

    simplified SMO algorithm

--- MLIA/svmMLiA.py
+++ MLIA/svmMLiA.py
@@ -9,6 +9,9 @@ margin maximum with points with smallest margin:
     max(a)[sigma(1, m){a} - sigma(i,j=1,, m){label(i)*label(j)*ai*aj*<x(i),x(j)}/2]
     with a>=0 and sigma(i-1, m){ai*label(i)} = 0
 '''
+
+from numpy import *
+
 # Helper function for SMO (Sequential Minimal Optimization) algorithm
 def loadDataSet(fileName):
     dataMat = []
@@ -45,3 +48,80 @@ While the number of iterations is less than MaxIterations:
             If the vector cannot be optimized -> break
     If no vector were optimized -> increse the iteration count
 '''
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    b = 0
+    m, n = shape(dataMatrix)
+    alphas = mat(zeros((m, 1)))
+    iterance = 0
+    while(iterance < maxIter):
+        alphaPairsChanged = 0
+        for i in range(m):
+            fXi = float(multiply(alphas, labelMat).T*\
+                       (dataMatrix*dataMatrix[i,:].T)) + b
+            Ei = fXi - float(labelMat[i])
+            # Enter optmiz if alphas can be changed
+            if((labelMat[i]*Ei < -toler) and (alphas[i] < C)) or \
+                ((labelMat[i]*Ei > toler) and \
+                (alphas[i] > 0)):
+                # randomly select second alpha
+                j = selectJrand(i, m)
+
+                fXj = float(multiply(alphas, labelMat).T*\
+                           (dataMatrix*dataMatrix[j,:].T)) + b
+                Ej = fXj - float(labelMat[j])
+                alphaIold = alphas[i].copy()
+                alphaJold = alphas[j].copy()
+                # Guarentee alphas stay between 0 and C
+                if(labelMat[i] != labelMat[j]):
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, C + alphas[i] - alphas[i])
+                else:
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, alphas[j] + alphas[i])
+                if L == H:
+                    print "L == H"
+                    continue
+                eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - \
+                      dataMatrix[i, :] * dataMatrix[i, :].T - \
+                      dataMatrix[j, :] * dataMatrix[j, :].T
+                if eta > 0:
+                    print "eta>=0"
+                    continue
+
+                # Update i by same amount as j in opposite direction
+                alphas[j] -= labelMat[j] * (Ei - Ej)/eta
+                alphas[j] = clipAlpha(alphas[j], H, L)
+                if(abs(alphas[i] - alphaJold) < 0.00001):
+                    print "j not moving enough"
+                    continue
+                alphas[i] += label[j] * label[i] * \
+                          (alphaJold - alphas[j])
+                # Set the constant terms
+                b1 = b - Ei - labelMat[i] * (alphas[i] - alphaIold) * \
+                     dataMatrix[i, :] * dataMatrix[i, :].T - \
+                     labelMat[j]*(alphas[j] - alphaJold) * \
+                     dataMatrix[i, :] * dataMatrix[j, :].T
+                b2 = b - Ej - labelMat[i] * (alphas[i] - alphaIold) * \
+                     dataMatrix[i, :] * dataMatrix[j, :].T - \
+                     labelMat[j]*(alphas[j] - alphaJold) * \
+                     dataMatrix[j, :] * dataMatrix[j, :].T
+                if(0 < alphas[i]) and (C > alphas[i]):
+                    b = b1
+                elif(0 < alphas[j]) and (C > alphas[j]):
+                    b = b2
+                else:
+                    b = (b1 + b2)/2.0
+                alphaPairsChanged += 1
+                print "iter: %d i:%d, pairs changed %d" % \
+                                  (iterance, i, alphaPairsChanged)
+        if(alphaPairsChanged == 0):
+            iterance += 1
+        else:
+            iterance = 0
+        print "iteration number: %d" % iterance
+    return b, alphas
+
+
+                
