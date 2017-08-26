39aa580d35c13b13bf5777676d1c51c423a36f66
Hai Tang <402696415@qq.com>
2017-08-25 23:14:37 -0700

    plot regression line

--- MLIA/regression.py
+++ MLIA/regression.py
@@ -18,8 +18,24 @@ def standRegres(xArr, yArr):
     xMat = mat(xArr)
     yMat = mat(yArr).T
     xTx = xMat.T * xMat
-    if linalg.det(xTx) == 0.0:
+    if linalg.det(xTx) == 0.0: # get determinant
         print "This matrix is singular, cannot do inverse"
         return
     ws = xTx.I * (xMat.T * yMat)
     return ws
+
+def plotRegres(fileName):
+    import matplotlib.pyplot as plt
+    xArr, yArr = loadDataSet(fileName)
+    ws = standRegres(xArr, yArr)
+    xMat = mat(xArr)
+    yMat = mat(yArr)
+    yHat = xMat * ws
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xMat[:,1].flatten().A[0], yMat.T[:,0].flatten().A[0])
+    xCopy = xMat.copy()
+    xCopy.sort(0)
+    yHat = xCopy * ws
+    ax.plot(xCopy[:,1],yHat)
+    plt.show()
