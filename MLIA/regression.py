a36567e9f8ca984687961435b232eaec6b636890
Hai Tang <402696415@qq.com>
2017-08-31 21:58:03 -0700

    stagewise regression and regularize

--- MLIA/regression.py
+++ MLIA/regression.py
@@ -140,8 +140,43 @@ For every iteration:
             Calculate the error with new W
             If Error is lower than lowestError: 
                 set Wbest to current W
-        Update set W to Wbest
+    Update set W to Wbest
 '''
+def stageWise(xArr, yArr, eps = 0.01, numIt = 100):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMat = regularize(xMat)
+    m, n = shape(xMat)
+    ws = zeros((n, 1))
+    wsTest = ws.copy()
+    wsMax = ws.copy()
+    for i in range(numIt):
+        print ws.T
+        lowestError = inf
+        for j in range(n):
+            for sign in [-1, 1]:
+                wsTest = ws.copy()
+                wsTest[j] += eps * sign
+                yTest = xMat * wsTest
+                rssE = rssError(yMat.A, yTest.A)
+                if rssE < lowestError:
+                    lowestError = rssE
+                    wsMax = wsTest
+        ws = wsMax.copy()
+        returnMat[i, :] = ws.T
+    return returnMat
+
+# regularize by columns
+def regularize(xMat):
+    inMat = xMat.copy()
+    inMeans = mean(inMat, 0) # mean of columns
+    inVar = var(inMat, 0)
+    inMat = (inMat - inMeans) / inVar
+    return inMat
+
+
 
 def plotLWLR():
     xArr, yArr = loadDataSet('ex0.txt')
