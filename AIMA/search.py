14913194bc42ad4f8371d2a1a8618c5c012ee403
Hai Tang <402696415@qq.com>
2017-09-19 22:26:16 -0700

    Node class

--- AIMA/search.py
+++ AIMA/search.py
@@ -10,14 +10,62 @@ import random
 import sys
 import bisect
 
+class Problem(object):
+    """Abstract class. Should be subclassed."""
+    def __init__(self, initial, goal=None):
+        self.initial = initial
+        self.goal = goal
+
+    def actions(self, state):
+        """Return the actions that can be executed in the given
+        state."""
+        return NotImplementedError
+
+    def result(self, state, action):
+        
+
 class Node:
     """
     A node in search tree
     """
     def __init__(self, state, parent=None, action=None, path_cost=0):
-        """Create a search tree node"""
+        """Create a search tree node."""
         self.state = state
         self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
+        if parent:
+            self.path = parent.depth + 1
+    
+    def __repr__(self):
+        return "<Node {}>".format(self.state)
+
+    def __lt__(self, node):
+        return self.state < node.state
+
+    def expand(self, problem):
+        """List the nodes reachable in one step from this node."""
+        return [self.child_node(problem, action)
+                    for action in problem.actions(self.state)]
+
+    def child_node(self, problem, action):
+        """[Figure 3.10]"""
+        next = problem.result(self.state, action)
+        return Node(next, self, action, 
+                    problem.path_cost(self.path_cost, self.state,
+                                      action, next))
+    def solution(self):
+        """Return the sequence of action to go to root"""
+        return [node.action for node in self.path()[1:]]
+
+    def path(self):
+        """Return a list of nodes in the path"""
+        node, path_back = self, []
+        while node:
+            path_back.append(node)
+            node = node.parent
+        return list(reversed(path_back))
 
 
 def hill_climbing(problem):
