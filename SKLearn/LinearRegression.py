48b658d73e32d048efb439c0971c31cfd8116f45
Hai Tang <402696415@qq.com>
2017-10-10 23:56:40 -0700

    LinearRegression

--- SKLearn/LinearRegression.py
+++ SKLearn/LinearRegression.py
@@ -3,6 +3,10 @@ from sklearn.linear_model import LinearRegression
 
 X = [[6], [8], [10], [14], [18]]
 y = [[7], [9], [13], [17.5], [18]]
+# Create and fit the model
+model = LinearRegression()
+model.fit(X, y)
+print('A 12" pizza should cost: %.2f' % model.predict([12])[0])
 plt.figure()
 plt.title('Pizza price plotted against diameter')
 plt.xlabel('Diameter in inches')
